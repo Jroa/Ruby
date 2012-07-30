0bb7fa45e59ed44f7ec3f129b9536adffcbcabb3
Jonathan Roa <jroa3000@gmail.com>
2012-07-29 19:03:09 -0500

    Ejemplo de codeSchool

---
+++ zombie/spec/lib/zombie_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+require "zombie"
+
+describe Zombie do 
+	it "is named Ash" do
+		zombie = Zombie.new
+		zombie.name.should == 'Ash'
+	end
+
+	it "has no brains" do
+		zombie = Zombie.new
+		zombie.brains.should < 1
+	end
+
+	it "is hungry" do
+		zombie = Zombie.new
+		#zombie.hungry?.should == true
+		#zombie.hungry?.should be_true
+		zombie.should be_hungry
+	end
+end
