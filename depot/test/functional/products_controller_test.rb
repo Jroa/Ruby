59f0102b9ef0b7e62f9dca4bc9cdf3c3d6ed1d19
Jonathan <jonathan@Teletran.(none)>
2012-11-11 14:03:02 -0500

    validaciones

--- depot/test/functional/products_controller_test.rb
+++ depot/test/functional/products_controller_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
+
   setup do
     @product = products(:one)
+    @update = {
+      :title        => 'Lorem Ipsum',
+      :description  => 'wibbles are fun',
+      :image_url    => 'lorem.jpg',
+      :price        => 19.95
+    }
   end
 
   test "should get index" do
@@ -18,7 +25,8 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+      post :create, :product => @update
+      #post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +43,8 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    #put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
+    put :update, :id => @product.to_param, :product => @update
     assert_redirected_to product_path(assigns(:product))
   end
 
